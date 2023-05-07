76a6dbc41bf7bf89b99e8bc3c2c914e6eb580349
Zettisch Márk <public@zetmark.hu>
2023-05-07 16:38:59 +0200

    [Add] Projekt létrehozás + Bejelentkezés front-end
    Java projekt létrehozása + Bejelentkezés form front-end elkészítése

---
+++ program/src/LoginPage.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+public class LoginPage extends JFrame {
+
+    private JPanel panelmain;
+    private JPanel mainPanel;
+    private JPanel text;
+    private JPanel form;
+    private JTextField username;
+    private JButton login_btn;
+    private JPasswordField password;
+    private JButton reg_button;
+
+    public LoginPage() {
+        setIconImage(new ImageIcon("login.png").getImage());
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(500, 300));
+        setLocationRelativeTo(null);
+        setVisible(true);
+        add(mainPanel);
+
+        String username_show_text = "Felhasználónév";
+        username.setText(username_show_text);
+        username.setForeground(Color.GRAY);
+
+        String password_show_text = "Jelszó";
+        password.setText(password_show_text);
+        password.setForeground(Color.GRAY);
+        password.setEchoChar((char)0);
+
+        username.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (username.getText().equals(username_show_text)) {
+                    username.setText("");
+                    username.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (username.getText().isEmpty()) {
+                    username.setForeground(Color.GRAY);
+                    username.setText(username_show_text);
+                }
+            }
+        });
+
+
+        password.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (password.getText().equals(password_show_text)) {
+                    password.setText("");
+                    password.setEchoChar('*');
+                    password.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (password.getText().isEmpty()) {
+                    password.setForeground(Color.GRAY);
+                    password.setEchoChar((char)0);
+                    password.setText(password_show_text);
+                }
+            }
+        });
+    }
+
+
+}
